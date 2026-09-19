980155d6e62594c392527ab3c6446ee6fcb16ea5
ghost-raider-afk <michaelfedoseev2978@gmail.com>
2026-09-19 19:41:13 +1000

    release: MIRA-TV 1.11.0

--- mira-tv.sh
+++ mira-tv.sh
@@ -2,7 +2,7 @@
 set -Eeuo pipefail
 
 PROGRAM_NAME="MIRA-TV"
-SCRIPT_VERSION="1.10.2"
+SCRIPT_VERSION="1.11.0"
 INSTALL_DIR="/opt/MIRA-TV"
 PERSIST_DIR="/var/lib/mira-tv"
 PERSIST_ENV="${PERSIST_DIR}/install.env"
