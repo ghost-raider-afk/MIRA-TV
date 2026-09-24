b90407964bfd96f9ebb0dcd081dcd02accec9930
ghost-raider-afk <michaelfedoseev2978@gmail.com>
2026-09-24 15:20:51 +1000

    release: bump installer version 1.14.2

--- mira-tv.sh
+++ mira-tv.sh
@@ -2,7 +2,7 @@
 set -Eeuo pipefail
 
 PROGRAM_NAME="MIRA-TV"
-SCRIPT_VERSION="1.14.1"
+SCRIPT_VERSION="1.14.2"
 INSTALL_DIR="/opt/MIRA-TV"
 PERSIST_DIR="/var/lib/mira-tv"
 PERSIST_ENV="${PERSIST_DIR}/install.env"
